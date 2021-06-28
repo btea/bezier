c44e47c59f42cf39ad2cbed4c19bee1bdbfa8bc8
btea <2356281422@qq.com>
2021-06-28 21:39:53 +0800

    chore: eslint add

--- src/utils/fun.ts
+++ src/utils/fun.ts
@@ -1,6 +1,6 @@
 export default function numConvert(n: number): number {
     if (!Number(n)) return 0;
-    let _s: string = String(n);
+    let _s = String(n);
     if (!/\./.test(_s)) return n;
     _s = n.toFixed(2);
     if (/0$/.test(_s)) return +_s.slice(0, -1);
@@ -10,22 +10,21 @@ type p = {
     x: number;
     y: number;
 };
-export function bezierFun(one: p, two: p) {
-    let a: number, b: number, c: number, d: number;
-    a = numConvert(one.x / 300);
-    b = numConvert((450 - one.y) / 300);
-    c = numConvert(two.x / 300);
-    d = numConvert((450 - two.y) / 300);
+export function bezierFun(one: p, two: p): string {
+    const a: number = numConvert(one.x / 300);
+    const b = numConvert((450 - one.y) / 300);
+    const c = numConvert(two.x / 300);
+    const d = numConvert((450 - two.y) / 300);
     return `${a}, ${b}, ${c}, ${d}`;
 }
 
-export function titleIconSet(title: string) {
+export function titleIconSet(title: string): void {
     title = `cubic-bezier(${title})`;
     // let icon = red.el.toDataURL('image/png');
-    let icon = '';
-    let img = document.createElement('img');
+    const icon = '';
+    const img = document.createElement('img');
     img.src = icon;
-    let titleEl = document.getElementsByTagName('title')[0];
+    const titleEl = document.getElementsByTagName('title')[0];
     let iconEl = document.getElementsByTagName('link')[0];
     if (!iconEl) {
         iconEl = document.createElement('link');
@@ -35,10 +34,10 @@ export function titleIconSet(title: string) {
     }
     titleEl.innerText = title;
     img.onload = () => {
-        let canvas = document.createElement('canvas');
+        const canvas = document.createElement('canvas');
         canvas.width = 60;
         canvas.height = 60;
-        let ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
         ctx.beginPath();
         ctx.fillStyle = '#6cf';
         ctx.rect(0, 0, 60, 60);
